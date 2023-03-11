ad34baa17a2081307cbab6c964b8ed5a8918f8dc
fmsfdd <115669093+fmsfdd@users.noreply.github.com>
2023-03-11 01:39:04 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+Config.StartPedLoc = vector4(-365.16, -1936.85, 21.5, 17.03) --ped loc
+
+Config.KartSpawn = vector4(-347.25, -1939.87, 21.6, 223.53) --kart spawn loc
+
+Config.StartPedAnimation = "WORLD_HUMAN_AA_COFFEE"
+
+Config.Timer = 1 -- minutes
+
+Config.Fuel = "ps-fuel"
+
+Config.KartModel = `veto2`
+
+Config.ReturnPos = vector3(-366.72, -1936.31, 21.31) --returning position
+
+Config.price = 200
+
+Config.locations = {
+    
+    vector4(-348.83, -1941.38, 21.6, 227.04),
+    vector4(-345.46, -1937.66, 21.6, 277.93),
+    vector4(-342.38, -1933.85, 21.6, 229.81),
+    vector4(-335.64, -1934.92, 21.6, 139.17),
+    vector4(-335.56, -1942.98, 21.6, 69.16),
+    vector4(-338.47, -1947.12, 21.6, 54.15),
+     
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+lua54 'yes'
